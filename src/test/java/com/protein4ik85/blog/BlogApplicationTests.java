26641af847566b21808531b7e08f0d1e3da8b84d
AndrewSih <93401804+AndrewSih@users.noreply.github.com>
2022-02-07 15:51:40 +0300

    Initial commit

---
+++ src/test/java/com/protein4ik85/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protein4ik85.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
